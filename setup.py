cd26a23b8ca4a8733c51a3e799e5924131edda29
Tobias Krabel <tobiaskrabel@Tobiass-MacBook-Pro-2.local>
2019-02-28 21:20:08 +0100

    init :sunglasses:

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'edavis',         
+  packages = ['edavis'],   
+  version = '0.0.0',     
+  license='',
+  description = 'Exploratory Data Analysis and Visualization',
+  author = 'Tobias Krabel',                   
+  author_email = 'tobiaskrabel@gmail.com',
+  url = 'https://github.com/tkrabel/edavis',
+  download_url = 'https://github.com/tkrabel/edavis/archive/0.0.0.tar.gz',
+  keywords = ['visualization', 'exploration', 'data analysis', 'data visualization'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
